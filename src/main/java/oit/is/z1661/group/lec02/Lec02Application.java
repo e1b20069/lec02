d5e8bdc8695ace12b6c916abccd48da933c05cf3
e1b20069 <e1b20069@st.oit.ac.jp>
2022-10-04 14:48:34 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z1661/group/lec02/Lec02Application.java
@@ -0,0 +1,13 @@
+package oit.is.z1661.group.lec02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lec02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lec02Application.class, args);
+	}
+
+}
